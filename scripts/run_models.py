f406cacf9b586d3cc275b183fff99e40dfaab263
ahartikainen <ahartikainen@github.com>
2020-10-30 22:09:49 +0200

    save errors

--- scripts/run_models.py
+++ scripts/run_models.py
@@ -20,38 +20,56 @@ model {
 }
 """
 
+
 def model_1():
     print("RUNNING MODEL 1")
-    schools_data = {"J": 8,
-                "y": [28,  8, -3,  7, -1,  1, 18, 12],
-                "sigma": [15, 10, 16, 11,  9, 11, 10, 18]}
+    schools_data = {
+        "J": 8,
+        "y": [28, 8, -3, 7, -1, 1, 18, 12],
+        "sigma": [15, 10, 16, 11, 9, 11, 10, 18],
+    }
 
     posterior = stan.build(schools_code, data=schools_data)
     fit = posterior.sample(num_chains=4, num_samples=1000)
     print(fit)
-    
+
+
 def model_2():
     print("RUNNING MODEL 2")
-    schools_data = {"J": 8*10,
-                "y": [28,  8, -3,  7, -1,  1, 18, 12]*10,
-                "sigma": [15, 10, 16, 11,  9, 11, 10, 18]*10}
+    schools_data = {
+        "J": 8 * 10,
+        "y": [28, 8, -3, 7, -1, 1, 18, 12] * 10,
+        "sigma": [15, 10, 16, 11, 9, 11, 10, 18] * 10,
+    }
 
     posterior = stan.build(schools_code, data=schools_data)
     fit = posterior.sample(num_chains=4, num_samples=1000)
     print(fit)
-    
+
 
 def model_3():
     print("RUNNING MODEL 3")
-    schools_data = {"J": 8*11,
-                "y": [28,  8, -3,  7, -1,  1, 18, 12]*11,
-                "sigma": [15, 10, 16, 11,  9, 11, 10, 18]*11}
+    schools_data = {
+        "J": 8 * 11,
+        "y": [28, 8, -3, 7, -1, 1, 18, 12] * 11,
+        "sigma": [15, 10, 16, 11, 9, 11, 10, 18] * 11,
+    }
 
     posterior = stan.build(schools_code, data=schools_data)
     fit = posterior.sample(num_chains=4, num_samples=1000)
     print(fit)
-    
+
+
 if __name__ == "__main__":
-    model_1()
-    model_2()
-    model_3()
+    try:
+        model_1()
+    except:
+        print("FAILURE 1")
+    try:
+        model_2()
+    except:
+        print("FAILURE 2")
+    try:
+        model_3()
+    except:
+        print("FAILURE 3")
